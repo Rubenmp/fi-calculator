56a3aef24d0b5c905e229f409282d40d0c41f381
Ruben <rbnmp@protonmail.com>
2021-08-06 00:41:23 +0200

    First connection between js and html

--- src/main.js
+++ src/main.js
@@ -1,4 +1,1 @@
-let d = new Date();
-d
-// document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
-document.getElementById("content").innerHTML = "whatever";
+document.getElementById("fi-calculator").innerHTML = ":)";
