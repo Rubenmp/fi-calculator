c3cfad6d0623a5e7062e2c4588690649291204c2
Ruben <rbnmp@protonmail.com>
2021-08-06 00:39:01 +0200

    Main html components

--- src/main.js
+++ src/main.js
@@ -1,2 +1,4 @@
 let d = new Date();
-document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+d
+// document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+document.getElementById("content").innerHTML = "whatever";
