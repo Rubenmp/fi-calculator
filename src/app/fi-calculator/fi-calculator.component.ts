ed7ba0cd0277ad4579f58cc7cf761e78b9c632db
Ruben <rbnmp@protonmail.com>
2021-08-08 20:30:07 +0200

    Link fi parameters emit with main component

--- src/app/fi-calculator/fi-calculator.component.ts
+++ src/app/fi-calculator/fi-calculator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FIParameters } from '../../model/fi-parameters';
 
 @Component({
   selector: 'fi-calculator',
@@ -10,6 +11,11 @@ export class FiCalculatorComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  onFIParameterChange(fiParameters: FIParameters) {
+    console.log(fiParameters);
   }
 
 }
